23dd3f94bab8a063ddcf309db6f2a9a1fbddd851
Md Aminour Islam Sayad <148259659+Aminourislam@users.noreply.github.com>
2025-01-13 18:26:27 +0600

    Update result.php

--- resultview.php
+++ resultview.php
@@ -11,7 +11,7 @@ $user_data = check_login($con);
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
     <title>Exam Results</title>
     <style type="text/css">
         body {
